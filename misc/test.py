826359f0d0b652523db5a8d73b713a975edb4e8e
Mohammad Amin Khakzadan <mak12776@gmail.com>
2019-04-05 13:42:23 +0430

    update misc

--- misc/test.py
+++ misc/test.py
@@ -14,14 +14,69 @@ def process(func):
                 )
     return process_init
 
-def main():
-    conn1, conn2 = multiprocessing.Pipe()
-    
-    conn1.send( (1, 2) )
-    conn1.close()
+class Workers:
+    def __init__(
+            self,
+            target,
+            inputs,
+            max_workers = _MAX_PROCESS_NUMBER,
+            ):
 
-    print(conn2.recv())
-    print(conn2.recv())
+        def worker_function(inital_job, inputs_conn, outputs_conn, *args):
+            try:
+                job = initial_job
+
+                while True:
+
+                    outputs_conn.send(target(job, *args))
+
+                    try:
+                        job = inputs_conn.recv()
+                    except EOFError:
+                        return
+
+            except KeyboardInterrupt:
+                pass
+
+
+        self._worker_function = process(worker_function)
+
+        self._iterable = iterable
+        self._workers = collections.deque()
+
+    def start_workers(self):
+        inputs_conn1, inputs_conn2 = multiprocessing.Pipe(False)
+        outputs_conn1, outputs_conn2 = multiprocessing.Pipe(False)
+
+        for item in self._iterable:
+            inputs_conn1.send(item)
+
+        inputs_conn1.close()
+
+        while len(self._workers) < _MAX_PROCESS_NUMBER:
+
+            try:
+                item = inputs_conn1.recv()
+            except EOFError:
+                break
+
+            self._workers.append(worker)
+
+            worker.start()
+
+def hash_worker(path):
+    hasher = hashlib.sha256()
+    with open(path, 'rb', 0) as infile:
+        _buffer = infile.read(io.DEFAULT_BUFFER_SIZE)
+        while _buffer:
+            hasher.update(_buffer)
+            _buffer = infile.read(io.DEFAULT_BUFFER_SIZE)
+    return hasher.digect()
+
+def main():
+    workers = Workers(
+            target = hash_worker,
+
 
 if __name__ == '__main__':
     main()
