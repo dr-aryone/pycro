5f0c5e4807ddaa0ced6df4235595f4b757d2a45c
Mohammad Amin Khakzadan <mak12776@gmail.com>
2019-04-06 22:57:49 +0430

    update test.py

--- misc/test.py
+++ misc/test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import random
 import time
 import multiprocessing
 import glob
@@ -10,6 +9,35 @@ import io
 import sys
 import os
 import queue
+import itertools
+import marshal
+import threading
+
+_fdopen = open
+_SIZE_LEN = 4
+
+def _write_size(outfile, size):
+    outfile.write(size.to_bytes(_SIZE_LEN, 'big', signed=False))
+
+def _read_size(infile):
+    _buffer = infile.read(_SIZE_LEN)
+    if len(_buffer) != _SIZE_LEN:
+        raise EOFError("End of file while reading size")
+    return int.from_bytes(_buffer, 'big', signed = False)
+
+def _read_buffer(infile, size):
+    _buffer = infile.read(size)
+    if len(_buffer) != size:
+        raise EOFError("End of file while reading buffer")
+    return _buffer
+    
+def _write_object(obj, outfile):
+    _buffer = marshal.dumps(obj, 4)
+    _write_size(len(_outfile, _buffer))
+    _outfile.write(_buffer)
+
+def _read_object(infile):
+    return marshal.loads(_read_buffer(infile, _read_size(infile)), 4)
 
 def process(func):
     def process_init(*args, **kwargs):
@@ -21,6 +49,31 @@ def process(func):
                 )
     return process_init
 
+class _WriterThread(threading.Thread):
+    def __init__(self, obj, outfile):
+        super().__init__(self)
+        self._obj = obj
+        self._outfile = outfile
+
+    def run(self):
+        _buffer = marshal.dumps(self._obj, 4)
+        _write_size(len(self._outfile, _buffer))
+        self._outfile.write(_buffer)
+
+class _ReaderThread(threading.Thread):
+    def __init__(self, obj, infile):
+        super().__init__(self)
+        self._obj = obj
+        self._infile = infile
+
+    def run(self):
+        return marshal.loads(
+                    _read_buffer(
+                        self._infile, 
+                        _read_size(infile)
+                        )
+                )
+
 class Queue:
     def __init__(self, maxsize = 0):
         if not isinstance(maxsize, int):
@@ -28,8 +81,8 @@ class Queue:
         if maxsize < 0:
             raise ValueError('maxsize must be greater than equal to 0')
 
-        self._pipe_r, self._pipe_w = map(
-                __fdopen, zip(os.pipe(), ('rb', 'wb')))
+        self._pipe_r, self._pipe_w = itertools.starmap(
+                _fdopen, zip(os.pipe(), ('rb', 'wb')))
 
         self._maxsize = maxsize
         self._size = 0
@@ -40,20 +93,21 @@ class Queue:
             if self._maxsize and self._size >= self._maxsize:
                 raise queue.Full()
             self._size += 1
-            marshal.dump(obj, self._pipe_w, 4)
+            _write_object(self._pipe_w, obj)
 
     def get(self):
         with self._rlock:
             if self._size:
+                print('self._size:', self._size)
                 self._size -= 1
-                return marshal.load(obj)
+                return _read_object(self._pipe_r)
             else:
                 raise queue.Empty()
 
 class Workers:
     def __init__(
             self, 
-            function,
+            target,
 
             args = (), 
             kwargs = {},
@@ -66,7 +120,7 @@ class Workers:
                 initial_item, inputs_queue, outputs_queue, *args, **kwargs):
             item = initial_item
             while True:
-                outputs_queue.put(function(item, *args, **kwargs))
+                outputs_queue.put(target(item, *args, **kwargs))
                 try:
                     item = inputs_queue.get()
                 except queue.Empty:
@@ -77,7 +131,9 @@ class Workers:
         self._kwargs = kwargs
 
         if max_workers is None:
-            self._max_workers = os.sched_getaffinity(0)
+            max_workers = len(os.sched_getaffinity(0))
+            if max_workers == 0:
+                raise OSError('os.sched_getaffinity(0) returns empty set')
 
         else:
             if not isinstance(max_workers, (int, None)):
@@ -86,14 +142,15 @@ class Workers:
             if max_workers <= 0:
                 raise ValueError(
                     'max_workers argument must be greater than equal to zero')
-            self._max_workers = max_workers
+
+        self._max_workers = max_workers
 
         self._workers = collections.deque()
 
         self._inputs_queue = Queue()
         if inputs is not None:
             for item in inputs:
-                self._inputs_queue.append(item)
+                self._inputs_queue.put(item)
 
         self._outputs_queue = Queue()
 
@@ -128,29 +185,7 @@ class Workers:
         for item in inputs:
             self._inputs_queue.put(item)
 
-        while True:
-            if len(self._workers) >= self._max_workers:
-                i = 0
-                while i < len(self._workers):
-                    if worker.is_alive():
-                        self._workers.pop(i)
-                    else:
-                        i += 1
-
-            try:
-                initial_item = self._inputs_queue.get()
-            except queue.Empty:
-                break
-
-            worker = self._process_init(
-                    initial_item, 
-                    self._inputs_queue, 
-                    self._outputs_queue,
-                    )
-
-            self._workers.append(worker)
-
-            worker.start()
+        self.start_workers()
 
     def join(self, timeout=None):
         if timeout is None:
@@ -163,6 +198,16 @@ class Workers:
                 if time.time() >= deadline:
                     return
                 timeout = time.time() - deadline
+
+    @property
+    def outputs(self):
+        outputs = collections.deque()
+        while True:
+            try:
+                item = self._outputs_queue.get()
+            except queue.Empty:
+                return outputs
+            outputs.append(item)
 
 def hash_worker(path):
     hasher = hashlib.sha256()
@@ -184,7 +229,8 @@ def main():
         print('[ parallel hashing ]')
 
         workers = Workers(
-                hash_worker)
+                hash_worker,
+                inputs = inputs)
 
         workers.start_workers()
         print('workers started')
